23d971fdd8fe9b8f42d8d3e78cd17de4d749dd7d
arlyxiao <kingla_pei@163.com>
2021-06-20 11:56:27 +0800

    Remove useless

--- parcel-reactjs/src/index.tsx
+++
@@ -1,8 +0,0 @@
-import * as React from 'react';
-import * as ReactDOM from "react-dom";
-
-import App from './app';
-import "./style.scss";
-
-var mountNode = document.getElementById("app");
-ReactDOM.render(<App name="Minecraft World" />, mountNode);
